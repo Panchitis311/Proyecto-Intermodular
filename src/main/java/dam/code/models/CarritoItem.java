0f1c3e01ba3afba08d2b38711f69e53f25b32af1
Panchitis311 <panchogomez2015@gmail.com>
2026-04-20 12:43:50 +0200

    Modeloos, cryptpassword y DatabaseConfig añadido

--- src/main/java/dam/code/models/CarritoItem.java
+++ src/main/java/dam/code/models/CarritoItem.java
@@ -1,4 +1,53 @@
 package dam.code.models;
 
 public class CarritoItem {
+    private int id;
+    private Producto producto;
+    private int cantidad;
+    private double precioUnitario;
+
+    public CarritoItem(Producto producto, int cantidad, double precioUnitario) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+    }
+
+    public CarritoItem(int id, Producto producto, int cantidad, double precioUnitario) {
+        this.id = id;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
 }
--- src/main/java/dam/code/models/Usuario.java
+++ src/main/java/dam/code/models/Usuario.java
@@ -1,4 +1,97 @@
 package dam.code.models;
 
+import dam.code.models.utils.Rol;
+
 public class Usuario {
+    private int id;
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private int telefono;
+    private Rol rol;
+
+    public Usuario(String dni, String nombre, String apellidos, String email, int telefono) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.telefono = telefono;
+        this.rol = Rol.USER;
+    }
+
+    public Usuario(String dni, String nombre, String apellidos, String email, int telefono, Rol rol) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.telefono = telefono;
+        this.rol = rol;
+    }
+
+    public Usuario(int id, String dni, String nombre, String apellidos, String email, int telefono, Rol rol) {
+        this.id = id;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.telefono = telefono;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
 }
